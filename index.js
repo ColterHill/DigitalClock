70c77c385e4aa8a1ab29c18507f09a45027b91a7
ColterHill <81501298+ColterHill@users.noreply.github.com>
2022-09-01 09:35:20 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,37 @@
+const hourEl = document.getElementById("hour");
+const minutesEl = document.getElementById("minutes");
+const secondsEl = document.getElementById ("seconds");
+const ampmEl = document.getElementById("ampm");
+const dateEl = document.getElementById("date");
+const dayEl = document.getElementById("day");
+
+function updateClock() {
+  let d = new Date().getUTCMonth();
+  let day = new Date().getDate();
+  let h = new Date().getHours();
+  let m = new Date().getMinutes();
+  let s = new Date().getSeconds();
+  let ampm = "AM";
+
+  if(h > 12) {
+    h = h - 12;
+    ampm = "PM"
+  }
+
+  h = h<10 ? "0" + h : h;
+  s = s<10 ? "0" + s : s;
+  m = m<10 ? "0" + m : m;
+
+
+  dateEl.innerText = d;
+  dayEl.innerText = day;
+  hourEl.innerText = h;
+  minutesEl.innerText = m;
+  secondsEl.innerText = s;
+  ampmEl, (innerText = ampm);
+  setTimeout(() => {
+    updateClock();
+  }, 1000);
+}
+
+updateClock();
